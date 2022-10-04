5292987a8a2c4ed75f0823563764f1d704b1eb67
Vadim Merenovskiy <101852782+RunninFridge@users.noreply.github.com>
2022-10-04 13:29:18 -0700

    added two functions instead on server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const Notes = require('./Develop/db/db.json');
+const { notStrictEqual } = require('assert');
 
 //set up for the Express to handle data parsing
 app.use(express.urlencoded({ extended: true}));
@@ -26,7 +27,49 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+//function to create new note
+function createNote(body, noteArray) {
+    const NewNote = body;
+    if (!Array.isArray(noteArray))
+    noteArray = [];
+    else if (noteArray.length === 0);
+    noteArray.push(0);
 
-function createNote()
+    body.id = noteArray[0];
+    noteArray[0]++;
 
-function deleteNote()
+    noteArray.push(NewNote);
+    fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(noteArray, null, 2)
+    );
+};
+
+app.post('/api/notes', (req, res) => {
+    const NewNote = createNote(req.body, Notes);
+    res.json(NewNote);
+});
+
+//function to delete new note
+function deleteNote(id, noteArray) {
+    for (let i = 0; i < noteArray.length; i++) {
+        let note = noteArray[i];
+        if (notStrictEqual.id == id) {
+            noteArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(noteArray, null, 2)
+            );
+            break;
+        };
+    };
+};
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, Notes);
+    res.json(true);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT} port!`);
+});
