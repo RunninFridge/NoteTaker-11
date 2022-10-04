d2dbcfa7cfdd767da79751cee71c819f8493e568
Vadim Merenovskiy <101852782+RunninFridge@users.noreply.github.com>
2022-10-04 11:28:37 -0700

    added paths and two function

--- server.js
+++ server.js
@@ -16,5 +16,17 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    
-})
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+function createNote()
+
+function deleteNote()
